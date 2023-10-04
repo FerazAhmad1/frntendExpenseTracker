0c6915beafc084801108ab7a705906042aa6c813
Feraz Khan <ferazahmadgit62@gmail.com>
2023-10-04 17:54:01 +0530

    login page

--- expensetrackerreact/src/components/SignupForm.js
+++ expensetrackerreact/src/components/SignupForm.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const SignupForm = ({ buttontype }) => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let name = nameRef.current.value ? nameRef.current.value.trim() : "";
+    let email = emailRef.current.value ? emailRef.current.value.trim() : "";
+    let password = passwordRef.current.value
+      ? passwordRef.current.value.trim()
+      : "";
+    if (!name) {
+      alert("please fill your name");
+      return;
+    }
+    if (!email) {
+      alert("please fill your email");
+      return;
+    }
+    if (!password) {
+      alert("please fill your password");
+      return;
+    }
+    console.log(name, email, password);
+  };
+
   return (
     <div className=" flex items-center justify-center min-h-screen min-w-full max-w-min p-4 border-4 border-yellow-300">
-      <form className=" flex flex-col p-10 border-2 border-blue-900 space-y-14">
-        <div className="flex space-x-2 justify-center items-center ">
-          <label className="font-extrabold text-3xl font-serif ">
-            UserName
-          </label>
-          <input
-            type="text"
-            className="p-4 text-3xl  outline-none border-2 rounded-3xl"
-            placeholder="username"
-          />
-        </div>
+      <form
+        onSubmit={submitHandler}
+        className=" flex flex-col p-10 border-2 border-blue-900 space-y-14">
+        {buttontype === "Signup" ? (
+          <div className="flex space-x-2 justify-center items-center ">
+            <label className="font-extrabold text-3xl font-serif ">
+              UserName
+            </label>
+            <input
+              ref={nameRef}
+              type="text"
+              className="  p-4 text-3xl  outline-none border-2 rounded-3xl"
+              placeholder="username"
+            />
+          </div>
+        ) : (
+          ""
+        )}
         <div className="flex space-x-2 justify-center items-center ">
           <label className="font-extrabold text-3xl font-serif ">Email</label>
           <input
+            ref={emailRef}
             type="text"
             className="p-4 text-3xl  outline-none border-2 rounded-3xl"
             placeholder="example@..."
@@ -28,6 +61,7 @@ export const SignupForm = ({ buttontype }) => {
             Password
           </label>
           <input
+            ref={passwordRef}
             className="p-4 text-3xl outline-none border-2 rounded-3xl"
             type="text"
             placeholder="password"
